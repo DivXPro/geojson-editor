51f8286cb3d486acc624cb84301f75fa578077da
Huhui <wishgood@gmail.com>
2019-08-09 02:02:17 +0800

    调整

--- src/components/marker-editor/marker-properties.js
+++ src/components/marker-editor/marker-properties.js
@@ -1,30 +1,74 @@
 import React, { useState } from 'react';
 import Immutable from 'immutable';
-import { Form, Input } from 'antd';
+import { Form, Input, InputNumber } from 'antd';
 
-export default function MarkerProperties(props) {
+function MarkerProperties(props) {
   const [editMarker, setEditMarker] = useState(props.marker);
 
-  const setProperty = (e, key) => {
-    const value = e.target.value;
+  const setProperty = (value, key) => {
     setEditMarker(Immutable.set(editMarker, key, value));
     props.onChange(key, value);
   }
 
+  const formLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 8 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 16 },
+    },
+    labelAlign: 'left',
+  };
+
+  const markProps = [
+    { key: 'id', name: 'ID', type: 'string', required: true },
+    { key: 'sid', name: '设备号', type: 'number' },
+    { key: 'type', name: '设备类型', type: 'string', required: true },
+    { key: 'url', name: '流媒体地址', type: 'string' },
+  ];
+
+  const { getFieldDecorator } = props.form;
+
   return (
-    <Form>
-      <Form.Item label="ID">
-        <Input value={editMarker.id} onChange={e => setProperty(e, 'id')}></Input>
-      </Form.Item>
-      <Form.Item label="设备编号">
-        <Input value={editMarker.sid} onChange={e => setProperty(e, 'sid')}></Input>
-      </Form.Item>
-      <Form.Item label="设备类型">
-        <Input value={editMarker.type} onChange={e => setProperty(e, 'type')}></Input>
-      </Form.Item>
-      <Form.Item label="流媒体地址">
-        <Input value={editMarker.url} onChange={e => setProperty(e, 'url')}></Input>
-      </Form.Item>
+    <Form {...formLayout}>
+      {
+        markProps.map(m =>
+          <Form.Item label={m.name} key={m.key}>
+            {
+              m.type === 'string' &&
+              getFieldDecorator(m.key, {
+                initialValue: editMarker[m.key],
+                rules: [
+                  {
+                    required: m.required,
+                    message: '必须输入值',
+                  },
+                ],
+              })(
+                <Input size="small" onChange={e => setProperty(e.target.value, m.key)}></Input>
+              )
+            }
+            {
+              m.type === 'number' &&
+              getFieldDecorator(m.key, {
+                initialValue: editMarker[m.key],
+                rules: [
+                  {
+                    required: m.required,
+                    message: '必须输入值',
+                  },
+                ],
+              })(
+                <InputNumber size="small" onChange={value => setProperty(value, m.key)} style={{ width: '100%' }}></InputNumber>
+              )
+            }
+          </Form.Item>
+        )
+      }
     </Form>
   );
 }
+
+export default Form.create()(MarkerProperties);
