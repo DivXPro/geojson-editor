b8e47530fb6cd997a627a23abba7d5fb2467c68d
Huhui <wishgood@gmail.com>
2019-08-20 01:39:49 +0800

    fix

---
+++ build/precache-manifest.380bb69bd5da75027c9e114aa51c0689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0854596d857bc1e829cde6cf13aac90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0adb87499de2d0ce9b65",
+    "url": "/static/css/2.fbd96d1b.chunk.css"
+  },
+  {
+    "revision": "ed78ac39c2dd924349c0",
+    "url": "/static/css/main.c88c7692.chunk.css"
+  },
+  {
+    "revision": "0adb87499de2d0ce9b65",
+    "url": "/static/js/2.4e4b2813.chunk.js"
+  },
+  {
+    "revision": "ed78ac39c2dd924349c0",
+    "url": "/static/js/main.d62ecce0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
